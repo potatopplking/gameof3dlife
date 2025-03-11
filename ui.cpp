f3b131060b9802802a2ac99dc74fba1ef50c4429
Jan Mrna <mj370@protonmail.com>
2025-03-11 21:18:10 +0100

    Move UI to separate file

---
+++ ui.cpp
@@ -0,0 +1,22 @@
+#include "ui.hpp"
+#include "utils.hpp"
+
+namespace UI {
+
+SphericCoords::SphericCoords(double phi, double theta, double r): coords{phi, theta, r} {
+    std::cout << "SphericCoords initalized: " << coords << std::endl;
+}
+
+utils::Vec<double, 3> SphericCoords::toCartesian() {
+    auto phi = coords[0] * M_PI / 180;
+    auto theta = coords[1] * M_PI / 180;
+    auto r = coords[2];
+
+    auto x = r * sin(phi) * cos(theta);
+    auto y = r * sin(phi) * sin(theta);
+    auto z = r * cos(phi);
+
+    return utils::Vec<double, 3>({x,y,z});
+}
+
+}
